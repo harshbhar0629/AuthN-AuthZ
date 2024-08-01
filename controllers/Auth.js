86c51970c7f07b703731961f228cf0a23c5e9e71
harshbhar0629 <harshbhar.0629@gmail.com>
2024-08-01 12:22:57 +0530

    Add login functionality

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -2,6 +2,8 @@
 
 const bcrypt = require("bcrypt");
 const User = require("../models/user.js");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // signup controller
 exports.signup = async (req, res) => {
@@ -65,42 +67,59 @@ module.exports.login = async (req, res) => {
 	//  I'm giving login functionality on the basis of email and password
 	try {
 		const { email, password } = req.body;
-		const data = await User.findOne({ email: email });
+		let data = await User.findOne({ email: email });
 
 		if (!data) {
-			return res.status(400).json({
+			return res.status(401).json({
 				success: false,
 				message: "User not registered before for this email",
 			});
 		}
 
 		// checking password
-		let hashed;
-		try {
-			// 2 values first kisko hash m convert krna h or second no of round used to convert into hash
-            hashed = await bcrypt.hash(password, 10);
-          
-		} catch (e) {
-			console.log("Error in conversion of password into hash");
-			return res.status(500).json({
-				success: false,
-				message: "Error in hashing conversion",
-			});
-		}
-        console.log(data.password);
-        console.log(hashed)
-		if (password === "" || data.password !== hashed) {
+		// console.log(data.password);
+
+		if (!password || !(await bcrypt.compare(password, data.password))) {
 			return res.status(500).json({
 				success: false,
 				message: "Incorrect password",
 			});
-		}
+		} else {
+			const payload = {
+				email: data.email,
+				id: data._id,
+				role: data.role,
+			};
+			let token = jwt.sign(payload, process.env.JWT_SECRET, {
+				expiresIn: "2h",
+			});
 
-		res.status(200).json({
-			success: true,
-			message: "User loged in successfully",
-			name: data.name,
-		});
+			// Way-1
+			// data = data.toObject();
+			// data.token = token;
+			// console.log(data.token);
+
+			// Way-2
+			// data._doc.token = token;
+			// console.log(typeof data);
+			// console.log(data)
+
+			// remove password
+			data.password = undefined;
+
+			const options = {
+				expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+				httpOnly: true
+			}
+
+			res.cookie("Cookie", token, options).status(200).json({
+				success: true,
+				token,
+				data,
+				message: "Successfully loged in",
+			});
+		}
+		
 	} catch (e) {
 		console.log("Error in login route");
 		return res.status(500).json({
@@ -109,3 +128,9 @@ module.exports.login = async (req, res) => {
 		});
 	}
 };
+
+
+// mongoDb - parent entity
+// databases - child entity
+// collections - child-ka-child entity
+// documents - {key-value}-pair
